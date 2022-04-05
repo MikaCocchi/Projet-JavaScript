af0c2e6f73367d051abf37513c898a1a9f789c89
Mika Cocchi <mika.cocchi@le-campus-numerique.fr>
2022-04-05 16:34:15 +0200

    fix: populateFeed function placed outside .done   feat: errors/ success messages

--- acceuil.js
+++ acceuil.js
@@ -12,33 +12,38 @@ $(document).ready(function () {
         //Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
         /*On peut par exemple convertir cette réponse en chaine JSON et insérer
          * cette chaine quelque part*/
+
         .done(function (response) {
             console.log(response);
+            $('.posts').prepend('<h3>tout est nickel</h3>');
+            $(".posts h3").delay(3000).fadeOut('slow', function () {
+                $(".posts h3").remove();
+            });
+
             if (response) {
                 populateFeed(response);
             }
-            function populateFeed(response) {
-                for (let i = 0; i < response.length; i++) {
-                    $('.posts').append('<div class="post"></div>');
-                    $('.posts .post:last').html('<img src="' + response[i].imageUrl + '"alt="Picsum Image" class="image">');
-                    $('.posts .post:last').prepend('<h3></h3>');
-                    $('.posts .post:last h3').prepend(response[i].title);
-                    $('.posts .post:last').append('<p></p>');
-                    $('.posts .post:last p').append(response[i].summary);
-                }
-            }
+
         })
-
-
 
         //Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
         //On peut afficher les informations relatives à la requête et à l'erreur
         .fail(function (error) {
-            alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+            $('.posts').prepend('<h3>IL Y A UNE ERREUR 0_0</h3>');
+            $(".posts h3").delay(3000).fadeOut('slow');
+            $(".posts h3").delay(3000).remove();
         })
-
-        //Ce code sera exécuté que la requête soit un succès ou un échec
-        .always(function () {
-            alert("Requête effectuée");
-        });
 });
+
+
+////////////FUNCTIONS/////////////////
+function populateFeed(response) {
+    for (let i = 0; i < response.length; i++) {
+        $('.posts').append('<div class="post"></div>');
+        $('.posts .post:last').html('<img src="' + response[i].imageUrl + '"alt="Picsum Image" class="image">');
+        $('.posts .post:last').prepend('<h3></h3>');
+        $('.posts .post:last h3').prepend(response[i].title);
+        $('.posts .post:last').append('<p></p>');
+        $('.posts .post:last p').append(response[i].summary);
+    }
+}
