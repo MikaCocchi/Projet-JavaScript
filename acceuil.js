41f14fcc55d759ba205c24919eb0e3f8f25fb499
Mika Cocchi <mika.cocchi@le-campus-numerique.fr>
2022-04-06 08:46:42 +0200

    fix ajax in function apiFeed

--- acceuil.js
+++ acceuil.js
@@ -1,38 +1,42 @@
 $(document).ready(function () {
-    $.ajax({
-        //L'URL de la requête 
-        url: "https://api.spaceflightnewsapi.net/v3/articles",
+    function apiFeed() {
+        $.ajax({
+            //L'URL de la requête 
+            url: "https://api.spaceflightnewsapi.net/v3/articles",
 
-        //La méthode d'envoi (type de requête)
-        method: "GET",
+            //La méthode d'envoi (type de requête)
+            method: "GET",
 
-        //Le format de réponse attendu
-        dataType: "json",
-    })
-        //Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
-        /*On peut par exemple convertir cette réponse en chaine JSON et insérer
-         * cette chaine quelque part*/
-
-        .done(function (response) {
-            console.log(response);
-            $('.posts').prepend('<h3>tout est nickel</h3>');
-            $(".posts h3").delay(3000).fadeOut('slow', function () {
-                $(".posts h3").remove();
-            });
-
-            if (response) {
-                populateFeed(response);
-            }
-
+            //Le format de réponse attendu
+            dataType: "json",
         })
+            //Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
+            /*On peut par exemple convertir cette réponse en chaine JSON et insérer
+             * cette chaine quelque part*/
 
-        //Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
-        //On peut afficher les informations relatives à la requête et à l'erreur
-        .fail(function (error) {
-            $('.posts').prepend('<h3>IL Y A UNE ERREUR 0_0</h3>');
-            $(".posts h3").delay(3000).fadeOut('slow');
-            $(".posts h3").delay(3000).remove();
-        })
+            .done(function (response) {
+                console.log(response);
+                $('.posts').prepend('<h3>tout est nickel</h3>');
+                $(".posts h3").delay(3000).fadeOut('slow', function () {
+                    $(".posts h3").remove();
+                });
+
+                if (response) {
+                    populateFeed(response);
+                }
+
+            })
+
+            //Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
+            //On peut afficher les informations relatives à la requête et à l'erreur
+            .fail(function (error) {
+                $('.posts').prepend('<h3>IL Y A UNE ERREUR 0_0</h3>');
+                $(".posts h3").delay(3000).fadeOut('slow');
+                $(".posts h3").delay(3000).remove();
+            })
+    }
+    //////////////CALL FUCTIONS/////////////////
+    apiFeed();
 });
 
 
@@ -47,3 +51,5 @@ function populateFeed(response) {
         $('.posts .post:last p').append(response[i].summary);
     }
 }
+
+
